59cadfbb2514fd38f62d63ddb3f70a44c2e6ac80
Gergely Topolyai <gerely01@gmail.com>
2016-12-03 18:36:49 +0100

    to gradle

--- ARMarker/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_BUILD_SCRIPT := $(call my-dir)/jni/Android.mk
-APP_PROJECT_PATH := $(call my-dir)
-APP_PLATFORM := 3
-APP_MODULES      :=  openCV
--- ARMarker/BuildTools/Build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-::Used to supress a warning. 
-set CYGWIN=nodosfilewarning
-
-%CYGWIN_PATH%\bin\bash --login -i %CD%\./compile.sh
